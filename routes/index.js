998c988cec3cbc0a0c62e2eb2a655ba494ab62fa
ynnep <adwpenalva@gmail.com>
2020-03-06 11:50:19 +0000

    views and routes structure done

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,22 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hello World!' });
 });
 
+router.post('/', (req, res, next) => {
+  const country = req.body.country;
+  res.render('form');
+});
+
 router.get('/form', (req, res, next) => {
   res.render('form');
 });
 
-router.get('/result', (req, res, next) => {
+router.post('/form', (req, res, next) => {
+  // const { data } = req.body.data;
   res.render('result');
 });
 
-router.post('/form', (req, res, next) => {});
+router.get('/result', (req, res, next) => {
+  res.render('result', { title: 'result page' });
+});
 
 module.exports = router;
