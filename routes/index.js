35f00b1c0632abd8b7440130479ddd245df80c20
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-03-06 10:51:52 +0000

    sem conflito

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 'use strict';
-
 const { Router } = require('express');
 const router = new Router();
-
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hello World!' });
 });
+router.get('/form', (req, res, next) => {
+  res.render('form');
+});
+router.get('/result', (req, res, next) => {
+  res.render('result')
+});
 
-router.get('/input', (req,res,next) => {
-  res.render('input');
-})
+router.post('/form', (req, res, next) => {
+  
+});
 
 module.exports = router;
